8bdcb7b497112c208f4979c7d200379b120238e5
MARIA TSEKOURA <maria.tsek@yahoo.gr>
2023-10-27 14:41:07 +0200

    updated NEXT_PUBLIC_ variable

--- pages/map.js
+++ pages/map.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Map, { Marker } from "react-map-gl";
-import { mapboxAccessToken } from "../secret.json";
+// import { mapboxAccessToken } from "../secret.json";
+
 import "mapbox-gl/dist/mapbox-gl.css";
 
 const mapStyle = {
   width: "50vw",
   height: "50vh",
 };
+
+const mapboxAccessToken = process.env.NEXT_PUBLIC_mapboxAccessToken;
+console.log("mapboxAccessToken", mapboxAccessToken);
 
 export default function MapBox({ longitude, latitude }) {
   return (
